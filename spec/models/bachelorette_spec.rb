9910e1ad7f869257bb2eea103d0350c6ab597f3a
Blake Sergesketter <blakesergesketter@gmail.com>
2024-03-15 18:45:42 -0500

    TEST- instance method model tested. All testing passes.

--- spec/models/bachelorette_spec.rb
+++ spec/models/bachelorette_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe Bachelorette, type: :model do
     it { should validate_presence_of :season_number }
     it { should validate_numericality_of :season_number }
   end
+
+  describe 'Instance methods' do 
+    it '#avg_ages' do 
+    bach = Bachelorette.create!(name: 'Hannah Brown', season_number: 15, season_description: 'The Most Dramatic Season Yet!')
+      
+    loki = Contestant.create!(name: 'Loki', age: 28, hometown: 'Evansville, IN', bachelorette_id: bach.id)
+    karl = Contestant.create!(name: 'Karl', age: 21, hometown: 'Evansville, IN', bachelorette_id: bach.id)
+
+    expect(bach.avg_ages).to eq(24.5)
+    end
+  end
 end
