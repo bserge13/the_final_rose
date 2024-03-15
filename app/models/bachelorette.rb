fbf11518cb4ea4e2c285cd47dfe09b59504c166f
Blake Sergesketter <blakesergesketter@gmail.com>
2024-03-14 19:49:44 -0500

    Initial commit.

--- app/models/bachelorette.rb
+++ app/models/bachelorette.rb
@@ -1,4 +1,5 @@
 class Bachelorette < ApplicationRecord
   has_many :contestants
-  
+  validates :name, presence: true 
+  validates :season_number, numericality: true 
 end
--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,4 +1,6 @@
 class Contestant < ApplicationRecord
   belongs_to :bachelorette
-  
+  validates :name, :hometown, presence: true
+  validates :age, numericality: true 
+
 end
